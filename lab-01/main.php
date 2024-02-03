d1591f0dd2a2048001862a2754e4a107fb2bed43
YuraPanov <yura03092004@gmail.com>
2024-02-03 20:22:08 +0200

    lab1

---
+++ lab-01/main.php
@@ -0,0 +1,5 @@
+<nav>
+    <hr><a href="https://www.youtube.com">Посмотри тут</a><br>
+    <a href="https://vk.com">Главная страница</a><br>
+    <a href="#">Раздел 1</a>
+    <a href="#">Раздел 2</a>
