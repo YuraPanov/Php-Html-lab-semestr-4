61a505c73593cfd6ef153e7f389f34eeb8051053
YuraPanov <yura03092004@gmail.com>
2024-02-04 10:01:13 +0200

    Find and fix all errors in the code. (Fixed)

--- lab-01/main.php
+++ lab-01/main.php
@@ -1,5 +1,25 @@
-<nav>
-    <hr><a href="https://www.youtube.com">Посмотри тут</a><br>
-    <a href="https://vk.com">Главная страница</a><br>
-    <a href="#">Раздел 1</a>
-    <a href="#">Раздел 2</a>
+<header>
+    <nav>
+        <a href="https://www.youtube.com">Посмотри тут</a><br>
+        <a href="https://vk.com">Главная страница</a><br>
+        <a href="#">Раздел 1</a>
+        <a href="#">Раздел 2</a>
+    </nav>
+    <hr>
+    <h1>Узнай на все 100 %!</h1>
+</header>
+
+<main>
+    <section>
+        <h1>Языки программирования</h1>
+        <figure>
+            <img src="https://www.python.org/static/img/python-logo.png"  alt=""/>
+            <figcaption>Ну смотрите питон</figcaption>
+        </figure>
+        <article>Изучать Python очень весело</article>
+        <p>Но нужно читать и практиковаться</p>
+        <p>И сильно-сильно стараться</p>
+    </section>
+</main>
+<footer>Успехов в освоении</footer>
+
