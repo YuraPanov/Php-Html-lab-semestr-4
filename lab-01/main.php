682aee6c8e5670ee109dcd97a5ffc5d1e8c38e4b
YuraPanov <yura03092004@gmail.com>
2024-02-05 18:05:54 +0200

    fixed

--- lab-01/main.php
+++ lab-01/main.php
@@ -1,25 +1,26 @@
-<header>
-    <nav>
-        <a href="https://www.youtube.com">Посмотри тут</a><br>
-        <a href="https://vk.com">Главная страница</a><br>
-        <a href="#">Раздел 1</a>
-        <a href="#">Раздел 2</a>
-    </nav>
-    <hr>
-    <h1>Узнай на все 100 %!</h1>
-</header>
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Узнай на все 100 %!</title>
+    </head>
 
-<main>
-    <section>
-        <h1>Языки программирования</h1>
-        <figure>
-            <img src="https://www.python.org/static/img/python-logo.png"  alt=""/>
-            <figcaption>Ну смотрите питон</figcaption>
-        </figure>
-        <article>Изучать Python очень весело</article>
-        <p>Но нужно читать и практиковаться</p>
-        <p>И сильно-сильно стараться</p>
-    </section>
-</main>
-<footer>Успехов в освоении</footer>
-
+    <body>
+        <section>
+            <h1>Языки программирования</h1>
+            <figure>
+                <img src="https://www.python.org/static/img/python-logo.png"  alt=""/>
+                <figcaption>Ну смотрите питон</figcaption>
+            </figure>
+            <article>Изучать Python очень весело</article>
+            <p>Но нужно читать и практиковаться</p>
+            <p>И сильно-сильно стараться</p>
+        </section>
+        <nav>
+            <a href="https://www.youtube.com">Посмотри тут</a><br>
+            <a href="https://vk.com">Главная страница</a><br>
+            <a href="#">Раздел 1</a>
+            <a href="#">Раздел 2</a>
+        </nav>
+        <footer>Успехов в освоении</footer>
+    </body>
+</html>
--- lab-02/main.php
+++ lab-02/main.php
@@ -1,23 +1,27 @@
+<!DOCTYPE html>
 <html lang="ru">
-    <!--Пример создании формы для выбора цвета в поле, используя "label" и "input"-->
-    <base href="https://cvetvibor.ru/" target="_blank">
-    <meta name="viewport" content="width=device-width, init-scale=1.0">
-    <header>
-        <h1>Выбири свой цвет</h1>
-    </header>
-    <style>
-        h1 {color: chocolate}
-        main {color: darkgoldenrod}
-    </style>
-    <main>
-        <form>
-            <label for="color">Цвет:</label>
-            <input list="colors" name="color" id="color">
-            <datalist id="colors">
-                <option value="Красный">
-                <option value="Синий">
-                <option value="Зелёный">
-            </datalist>
-        </form>
-    </main>
+    <head>
+        <title>Color</title>
+        <!--Пример создании формы для выбора цвета в поле, используя "label" и "input"-->
+        <base href="https://cvetvibor.ru/" target="_blank">
+        <meta name="viewport" content="width=device-width, init-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+        <header>
+            <h1>Выбири свой цвет</h1>
+        </header>
+        <main>
+            <form>
+                <label for="color">Цвет:</label>
+                <input list="colors" name="color" id="color">
+                <datalist id="colors">
+                    <option class="color1" value="Красный">
+                    <option class="color2" value="Синий">
+                    <option class="color3" value="Зелёный">
+                </datalist>
+            </form>
+        </main>
+    </body>
 </html>
