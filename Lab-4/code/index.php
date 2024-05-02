182001c3be086856264192e0dccddb070eedfb35
YuraPanov <yura03092004@gmail.com>
2024-05-02 23:01:25 +0200

    1

--- Lab-4/code/index.php
+++ Lab-4/code/index.php
@@ -1,85 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Avito</title>
+    <title>Working with online table</title>
 </head>
 <body>
-    <div id="form">
-        <form action="save.php" method="post">
-        <p>
-            <label for="email">Email</label>
-            <label>
-                <input type="email" name="email" required>
-            </label>
-        </p>
-        <p>
+<div id="submission">
+    <h1>Enter submission</h1>
+    <form method="post" action="save.php">
+        <label for="email">Email</label>
+        <input type="email" name="email" required><br>
+
         <label for="category">Category</label>
-            <label>
-                <select name="category" required>
-                    <?php
-                    $dirs = scandir("./categories");
-                    foreach ($dirs as $dir) {
-                        if ($dir != '.' && $dir != '..') {
-                            echo "<option value=\"$dir\">$dir</option>";
-                        }
-                    }
-                    ?>
-                </select>
-            </label>
-        </p>
-        <p>
-            <label for="title">Title</label>
-            <input type="title" name="title" required>
-        </p>
-        <label for="description">Description</label>
-        <p>
-            <label>
-                <textarea rows="3" name="description"></textarea>
-            </label>
-            <input type="submit" value="Save">
-        </p>
+        <select name="category" required>
+            <option value="cars">Business</option>
+            <option value="education">Education</option>
+            <option value="books">Finances</option>
+            <option value="family">Family</option>
+            <option value="other">Other</option>
+        </select><br>
 
-        </form>
-    </div>
-    <div id="table">
+        <label for="title">Title</label>
+        <input type="title" name="title" required><br>
+
+        <label for="content">Content</label>
+        <textarea id="content" name="content" rows="20" cols="120" required></textarea><br>
+
+        <input type="submit" name="submit_broadcast" value="SUBMIT">
+    </form>
+</div>
+<div id="table">
+    <h3>Uploaded submissions:</h3>
+    <table>
+        <thead>
+        <th>Category</th>
+        <th>Email</th>
+        <th>Title</th>
+        <th>Content</th>
+        </thead>
+        <tbody>
         <?php
-        function output($filePath, &$outputArray) {
-            $directories = array_slice(scandir($filePath), 2);
+        require_once "vendor/autoload.php";
 
-            foreach ($directories as $dir) {
-                $nextFilePath = $filePath . "/{$dir}";
+        $client = new Google_Client();
+        $client->setAuthConfig("credentials.json");
+        $client->addScope('https://www.googleapis.com/auth/spreadsheets');
+        $service = new Google_Service_Sheets($client);
 
-                if (is_dir($nextFilePath)) {
-                    $subDirs = array_slice(scandir($nextFilePath), 2);
-                    if (!empty($subDirs)) { // Проверка на пустую категорию
-                        output($nextFilePath, $outputArray);
-                    }
-                } elseif (mime_content_type($nextFilePath) == "text/plain") {
-                    $email = basename(dirname($nextFilePath, 2));
-                    $category = basename(dirname($nextFilePath));
-                    $title = substr($dir, 0, -4);
-                    $text = nl2br(file_get_contents($nextFilePath));
-                    $outputArray[] = "<tr><td>{$email}</td><td>{$category}</td><td>{$title}</td><td>{$text}</td></tr>";
+        $id = "1mG9OsC3pnvIn4DWTV1Hg700gP9j6RCZXu9OP_og_XuE";
+        $sheet = "лист1";
+        $table = $service->spreadsheets_values->get($id, $sheet)->getValues();
+
+        if ($table !== null) {
+            foreach($table as $row) {
+                echo "<tr>";
+                foreach($row as $cell) {
+                    echo "<td>{$cell}</td>";
                 }
+                echo "</tr>";
             }
         }
-        $outputArray = [];
-        if (is_dir("categories")) {
-            output("categories", $outputArray);
-        }
         ?>
-        <table border="2">
-            <tr>
-                <th>Category</th>
-                <th>Email</th>
-                <th>Title</th>
-                <th>Description</th>
-            </tr>
-            <?php echo implode("", $outputArray); ?>
-        </table>
-    </div>
-
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
--- Lab-4/code/save.php
+++ Lab-4/code/save.php
@@ -4,27 +4,46 @@ function redirectToHome(): void {
     exit();
 }
 
-if (!isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
+if (!isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['content'])) {
     redirectToHome();
 }
 
 $email = $_POST['email'];
 $category = $_POST['category'];
 $title = $_POST['title'];
-$desc = $_POST['description'];
+$desc = $_POST['content'];
 
+// Сохранение данных в текстовый файл
 $dirPath = "categories/{$category}/{$email}";
 if (!is_dir($dirPath)) {
-    mkdir($dirPath, 0777, true); // Создание директории и всех вложенных поддиректорий
+    mkdir($dirPath, 0777, true);
 }
 
 $filePath = "{$dirPath}/{$title}.txt";
-
-// Запись описания в файл
 if (false === file_put_contents($filePath, $desc)){
     throw new Exception("Ошибка при сохранении описания.");
 }
 
-chmod($filePath, 0777);
-redirectToHome();
+require_once "vendor/autoload.php";
+
+function goBack() {
+    header("Location: /");
+    exit();
+}
+
+$client = new Google_Client();
+$client->setAuthConfig("credentials.json");
+$client->addScope('https://www.googleapis.com/auth/spreadsheets');
+$service = new Google_Service_Sheets($client);
+
+$id = "1mG9OsC3pnvIn4DWTV1Hg700gP9j6RCZXu9OP_og_XuE";
+$sheet = "лист1";
+$table = [[$category, $email, $title, $desc]];
+
+$body = new Google_Service_Sheets_ValueRange(['values' => $table]);
+$params = array('valueInputOption' => 'USER_ENTERED');
+
+$service->spreadsheets_values->append($id, $sheet, $body, $params);
+
+goBack();
 ?>
